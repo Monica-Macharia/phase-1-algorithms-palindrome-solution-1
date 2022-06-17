c7d1924d9a1fd6b7d0c1d66c7b8c27b700bc54a0
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2022-06-17 06:14:19 +0300

    commit tree 130f23eb59d9fab2d8f2f585c4e768714044f773

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const transformString = word.split('').reverse().join('');
+  return word === transformString;
 }
 
 /* 
-  Add your pseudocode here
+  "abc" turned to an array ["a","b","c"],
+  ["a","b","c"] reversed into ["c","b","a"]
+  ["c","b","a"] joined back to a string "cba",
+  if "cba" === "abc" return true else false
 */
 
 /*
-  Add written explanation of your solution here
+  The function should return true if a word is a palindrome and false if not.
+  "abc" return false
+  "dad" return true
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +26,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("dad"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("dog"));
 }
 
 module.exports = isPalindrome;
